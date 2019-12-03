8d0fab48ad7ffb850973939e9c5ac59dd622f5aa
Darkerego <darkerego@users.noreply.github.com>
2019-12-03 17:08:33 -0500

    Update keypress_count.py

--- keypress_count.py
+++ keypress_count.py
@@ -1,5 +1,6 @@
 """
-Prior to having fancy iPhones, teenagers would wear out their thumbs sending SMS messages on candybar-shaped feature phones with 3x4 numeric keypads.
+Prior to having fancy iPhones, teenagers would wear out their thumbs sending SMS messages on 
+candybar-shaped feature phones with 3x4 numeric keypads.
 ------- ------- -------
 |     | | ABC | | DEF |
 |  1  | |  2  | |  3  |
@@ -17,15 +18,26 @@ Prior to having fancy iPhones, teenagers would wear out their thumbs sending SMS
 |  *  | |  0  | |  #  |
 ------- ------- -------
 
-Prior to the development of T9 (predictive text entry) systems, the method to type words was called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
+Prior to the development of T9 (predictive text entry) systems, the method to type words was 
+called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
 
-For example, to type a letter "R" you would press the 7 key three times (as the screen display for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user presses a different key or pauses for a short period of time (thus, no extra button presses are required beyond what is needed for each letter individually). The zero key handles spaces, with one press of the key producing a space and two presses producing a zero.
+For example, to type a letter "R" you would press the 7 key three times (as the screen display 
+for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user 
+presses a different key or pauses for a short period of time (thus, no extra button presses are 
+required beyond what is needed for each letter individually). The zero key handles spaces, with
+one press of the key producing a space and two presses producing a zero.
 
-In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button presses. No wonder they abbreviated.
+In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button 
+presses. No wonder they abbreviated.
 
-For this assignment, write a module that can calculate the amount of button presses required for any phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't distinguish between upper/lowercase characters (but you should allow your module to accept input in either for convenience).
+For this assignment, write a module that can calculate the amount of button presses required for any 
+phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't 
+distinguish between upper/lowercase characters (but you should allow your module to accept input 
+in either for convenience).
 
-Hint: While it wouldn't take too long to hard code the amount of keypresses for all 26 letters by hand, try to avoid doing so! (Imagine you work at a phone manufacturer who might be testing out different keyboard layouts, and you want to be able to test new ones rapidly.)
+Hint: While it wouldn't take too long to hard code the amount of keypresses for all 26 letters by 
+hand, try to avoid doing so! (Imagine you work at a phone manufacturer who might be testing out 
+different keyboard layouts, and you want to be able to test new ones rapidly.)
 """
 
 def presses(phrase):
